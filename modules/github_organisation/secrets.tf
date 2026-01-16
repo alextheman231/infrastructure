70d879acf06f7c3fbc3d8e3bda5b0ab87c46c8d8
AlexMan123456 <alex.man03888@gmail.com>
2026-01-16 23:54:25 +0000

    Also add the alex-up-bot app ID
    This will be needed for token minting in the actions that create pull requests.

--- modules/github_organisation/secrets.tf
+++ modules/github_organisation/secrets.tf
@@ -4,6 +4,12 @@ resource "github_actions_organization_secret" "alex_up_bot_github_token" {
   visibility      = "all"
 }
 
+resource "github_actions_organization_secret" "alex_up_bot_app_id" {
+  secret_name     = "ALEX_UP_BOT_APP_ID"
+  plaintext_value = var.alex_up_bot_app_id
+  visibility      = "all"
+}
+
 resource "github_actions_organization_secret" "alex_up_bot_private_key" {
   secret_name     = "ALEX_UP_BOT_PRIVATE_KEY"
   encrypted_value = var.alex_up_bot_private_key
--- modules/github_organisation/variables.tf
+++ modules/github_organisation/variables.tf
@@ -19,6 +19,12 @@ variable "alex_up_bot_private_key" {
   sensitive   = true
 }
 
+variable "alex_up_bot_app_id" {
+  description = "App ID for alex-up-bot, commonly used to create pull requests in GitHub Actions. This is intended to replace alex_up_bot_github_token as that is a PAT and less secure than a GitHub App. This should be used alongside alex_up_bot_private_key."
+  type        = string
+  sensitive   = true
+}
+
 variable "webhook_url" {
   description = "The URL of a webhook to receive notifications about my organisation."
   type        = string
