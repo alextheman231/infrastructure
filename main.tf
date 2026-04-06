7b19cbf15e585d2aa1e8ba050d01a08998476592
AlexMan123456 <alex.man03888@gmail.com>
2026-04-06 15:24:58 +0100

    Refactor GitHub modules into their own github folder
    This will scale better as I start adding the tfe configuration.

--- main.tf
+++ main.tf
@@ -28,7 +28,7 @@ provider "github" {
 }
 
 module "github_organisation" {
-  source                  = "./modules/github_organisation"
+  source                  = "./modules/github/organisation"
   billing_email           = var.billing_email
   alex_up_bot_app_id      = var.alex_up_bot_app_id
   alex_up_bot_private_key = var.alex_up_bot_private_key
