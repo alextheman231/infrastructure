a8834c7481723fa752485e4de927c6f6e1280cbd
AlexMan123456 <alex.man03888@gmail.com>
2026-05-02 01:34:21 +0100

    Replace encrypted_value with value_encrypted, and plaintext_value with just value

--- modules/github/repository/main.tf
+++ modules/github/repository/main.tf
@@ -175,5 +175,5 @@ resource "github_actions_secret" "default" {
   for_each        = nonsensitive(var.secrets)
   repository      = var.name
   secret_name     = each.key
-  encrypted_value = each.value
+  value_encrypted = each.value
 }
