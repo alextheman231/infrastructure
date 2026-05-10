af243cef4e254fa53a928c1daa79a5f2c36567c5
AlexMan123456 <alex.man03888@gmail.com>
2026-05-10 16:26:39 +0100

    Rename lexicon project on Vercel to lexicon-front-end
    This makes it clearer that it is managing purely the front-end. The back-end is planned to be deployed separately, either through Railway, or Neon and Render.

--- vercel.tf
+++ vercel.tf
@@ -8,7 +8,7 @@ module "vercel_team" {
 module "lexicon_project" {
   source           = "./modules/vercel/project"
   vercel_team_id   = var.vercel_team_id
-  name             = "lexicon"
+  name             = "lexicon-front-end"
   framework        = "vite"
   output_directory = "apps/front-end/dist"
 }
