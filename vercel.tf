d36bacdb620217c546db2e57b666482230e23991
AlexMan123456 <alex.man03888@gmail.com>
2026-05-10 14:09:19 +0100

    Move the Vercel team config to modules/vercel/team
    This will give space for modules/vercel/project

--- vercel.tf
+++ vercel.tf
@@ -1,5 +1,5 @@
 module "vercel_team" {
-  source         = "./modules/vercel"
+  source         = "./modules/vercel/team"
   vercel_team_id = var.vercel_team_id
   name           = "AlexTheMan"
   description    = "Vercel team for my personal alextheman organisation, to help with managing the deployment of my Turborepo apps."
