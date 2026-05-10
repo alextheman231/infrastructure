88fea26a8899af7e5bfa0545d154241fce1a0607
AlexMan123456 <alex.man03888@gmail.com>
2026-05-10 14:28:03 +0100

    Add the initial configuration for Lexicon deployment
    This is expected to create the project in Vercel but will not actually create a deployment. In order to do this we will need to create a GitHub Action in Lexicon. I would prefer to do that to give me more control over the deployment cycle.

---
+++ modules/vercel/project/variables.tf
@@ -0,0 +1,25 @@
+variable "name" {
+  description = "The name of the project"
+  type        = string
+}
+
+variable "framework" {
+  description = "The framework that is being used for the project"
+  type        = string
+}
+
+variable "vercel_team_id" {
+  description = "ID for my Vercel team"
+  type        = string
+}
+
+variable "output_directory" {
+  description = "The path to the built output of the thing to deploy"
+  type        = string
+}
+
+variable "build_command" {
+  description = "The command to use to build the project"
+  type        = string
+  default     = "pnpm run build"
+}
--- vercel.tf
+++ vercel.tf
@@ -4,3 +4,11 @@ module "vercel_team" {
   name           = "AlexTheMan"
   description    = "Vercel team for my personal alextheman organisation, to help with managing the deployment of my Turborepo apps."
 }
+
+module "lexicon_project" {
+  source           = "./modules/vercel/project"
+  vercel_team_id   = var.vercel_team_id
+  name             = "lexicon"
+  framework        = "vite"
+  output_directory = "apps/front-end/dist"
+}
