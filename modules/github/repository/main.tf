ddd8ebe0bf8fb8c4de273ecc2662993cf3dea354
AlexMan123456 <alex.man03888@gmail.com>
2026-05-10 14:59:51 +0100

    Add GitHub Actions variable resource
    This allows us to store GitHub Actions variables that don't necessarily need to be kept secret.

--- modules/github/repository/main.tf
+++ modules/github/repository/main.tf
@@ -176,3 +176,10 @@ resource "github_actions_secret" "default" {
   secret_name     = each.key
   value_encrypted = each.value
 }
+
+resource "github_actions_variable" "example_variable" {
+  for_each      = var.variables
+  repository    = var.name
+  variable_name = each.key
+  value         = each.value
+}
