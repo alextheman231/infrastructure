37bdc6970110b4a54bbe5b4aafc5168840f2ad60
AlexMan123456 <alex.man03888@gmail.com>
2026-05-10 21:16:23 +0100

    Add the Lexicon Render Deploy key to the Lexicon actions secrets

--- repositories.tf
+++ repositories.tf
@@ -147,6 +147,7 @@ module "lexicon_repository" {
     VERCEL_TOKEN      = var.vercel_api_token_lexicon_github
     VERCEL_ORG_ID     = var.vercel_team_id_lexicon_github
     VERCEL_PROJECT_ID = var.vercel_lexicon_project_id
+    RENDER_DEPLOY_KEY = var.lexicon_render_deploy_key
   }
 }
 
