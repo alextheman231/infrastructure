54947db372edc86a18fb20716d26200ee399bca7
AlexMan123456 <alex.man03888@gmail.com>
2026-05-11 21:34:57 +0100

    Add the new lexiconblogs.com domain as an allowed origin
    We are now using our own custom domain rather than the automatically generated one. This should be more stable and less prone to changes, therefore making it more suitable for CORS.

--- lexicon.tf
+++ lexicon.tf
@@ -30,6 +30,6 @@ module "lexicon_server" {
     DATABASE_URL    = var.lexicon_database_url
     NODE_ENV        = "production"
     API_BASE_URL    = var.lexicon_api_base_url
-    ALLOWED_ORIGINS = "https://lexicon-front-n9bwg11a6-alextheman.vercel.app"
+    ALLOWED_ORIGINS = "https://${var.lexicon_domain}"
   }
 }
