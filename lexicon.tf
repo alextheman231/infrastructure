f521754225303451957ce17b1aa71b92bc5b659f
AlexMan123456 <alex.man03888@gmail.com>
2026-05-12 01:45:09 +0100

    Change the Render deployment strategy to use pre-built Docker images
    As of now we deploy by having Render run the Dockerfile. This changes that so that we control how the Dockerfile gets run, which also allows us to bring the Dockerfile logs into the GitHub Actions workflow output, reducing the need to context-switch to Render to monitor the logs.

--- lexicon.tf
+++ lexicon.tf
@@ -18,6 +18,7 @@ module "lexicon_server" {
   source         = "./modules/render"
   name           = "Lexicon"
   repository_url = var.lexicon_repository_url
+  docker_image   = "${var.docker_username}/lexicon"
   secrets = {
     DATABASE_URL         = var.lexicon_database_url
     NODE_ENV             = "production"
