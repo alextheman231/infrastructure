bb76b0b1ee4cefa0e0a70f6bc3ce35d16f9bc5da
AlexMan123456 <alex.man03888@gmail.com>
2026-05-12 23:33:32 +0100

    Add moved blocks
    This will ensure that during the migration, Terraform doesn't try and actually recreate the resources, instead just mapping the old references to the new references.

---
+++ moved.tf
@@ -0,0 +1,94 @@
+moved {
+  from = module.alex_c_line_repository
+  to   = module.stacks.module.alex_c_line_repository
+}
+
+moved {
+  from = module.alex_g_bot_2_repository
+  to   = module.stacks.module.alex_g_bot_2_repository
+}
+
+moved {
+  from = module.components_repository
+  to   = module.stacks.module.components_repository
+}
+
+moved {
+  from = module.eslint_plugin_repository
+  to   = module.stacks.module.eslint_plugin_repository
+}
+
+moved {
+  from = module.github_actions_repository
+  to   = module.stacks.module.github_actions_repository
+}
+
+moved {
+  from = module.github_organisation
+  to   = module.stacks.module.github_organisation
+}
+
+moved {
+  from = module.infrastructure_repository
+  to   = module.stacks.module.infrastructure_repository
+}
+
+moved {
+  from = module.infrastructure_workspace
+  to   = module.stacks.module.infrastructure_workspace
+}
+
+moved {
+  from = module.lexicon_database
+  to   = module.stacks.module.lexicon_database
+}
+
+moved {
+  from = module.lexicon_image
+  to   = module.stacks.module.lexicon_image
+}
+
+moved {
+  from = module.lexicon_project
+  to   = module.stacks.module.lexicon_project
+}
+
+moved {
+  from = module.lexicon_repository
+  to   = module.stacks.module.lexicon_repository
+}
+
+moved {
+  from = module.lexicon_server
+  to   = module.stacks.module.lexicon_server
+}
+
+moved {
+  from = module.media_repository
+  to   = module.stacks.module.media_repository
+}
+
+moved {
+  from = module.neurosongs_2_repository
+  to   = module.stacks.module.neurosongs_2_repository
+}
+
+moved {
+  from = module.tfe_organisation
+  to   = module.stacks.module.tfe_organisation
+}
+
+moved {
+  from = module.typescript_actions_repository
+  to   = module.stacks.module.typescript_actions_repository
+}
+
+moved {
+  from = module.utility_repository
+  to   = module.stacks.module.utility_repository
+}
+
+moved {
+  from = module.vercel_team
+  to   = module.stacks.module.vercel_team
+}
