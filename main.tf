674daff11c0ff6a2470e8041b51c0fa5e8b57017
AlexMan123456 <alex.man03888@gmail.com>
2026-05-12 22:29:53 +0100

    Move the GitHub organisation configuration into its own file

--- main.tf
+++ main.tf
@@ -68,12 +68,3 @@ provider "docker" {
   username = var.docker_username
   password = var.docker_pat
 }
-
-module "github_organisation" {
-  source                  = "./modules/github/organisation"
-  billing_email           = var.billing_email
-  alex_up_bot_app_id      = var.alex_up_bot_app_id
-  alex_up_bot_private_key = var.alex_up_bot_private_key
-  webhook_url             = var.webhook_url
-}
-
