655e733945671b180779815882d46f00682affe8
AlexMan123456 <alex.man03888@gmail.com>
2026-05-12 22:29:53 +0100

    Move the GitHub organisation configuration into its own file

---
+++ github_organisation.tf
@@ -0,0 +1,7 @@
+module "github_organisation" {
+  source                  = "./modules/github/organisation"
+  billing_email           = var.billing_email
+  alex_up_bot_app_id      = var.alex_up_bot_app_id
+  alex_up_bot_private_key = var.alex_up_bot_private_key
+  webhook_url             = var.webhook_url
+}
