a505977900d062e1243aede6be55e127e6f7ff53
AlexMan123456 <alex.man03888@gmail.com>
2026-05-14 01:05:12 +0100

    Add the Sentry organisation token to the GitHub organisation secrets
    It's not technically Lexicon-specific as it can be used in other projects, but as of now it is only being used by Lexicon.

--- stacks/variables.tf
+++ stacks/variables.tf
@@ -204,3 +204,9 @@ variable "lexicon_back_end_sentry_dsn" {
   description = "The Sentry DSN for the Lexicon back-end."
   type        = string
 }
+
+variable "sentry_organisation_token" {
+  description = "Sentry organisation token to use in GitHub Actions. This has been encrypted with respect to the alextheman231 GitHub organisation."
+  type        = string
+  sensitive   = true
+}
