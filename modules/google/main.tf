2e20583d0c0d5eeb73df999743b3aea09e9bf358
AlexMan123456 <alex.man03888@gmail.com>
2026-06-04 00:21:10 +0100

    Reapply "Add the Google OAuth configuration to the Terraform configuration"
    This reverts commit f9a8bbfe6faa54f5600cfbad873c06af65e4674b.
    
    So I can investigate why this initially failed.

---
+++ modules/google/main.tf
@@ -0,0 +1,23 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = ">=7.35.0"
+    }
+  }
+}
+
+provider "google" {
+  credentials = var.credentials
+  project     = var.project_id
+}
+
+resource "google_iam_oauth_client" "default" {
+  allowed_scopes        = ["openid"]
+  allowed_grant_types   = ["AUTHORIZATION_CODE_GRANT"]
+  allowed_redirect_uris = var.allowed_redirect_uris
+  oauth_client_id       = var.google_client_id
+  location              = var.location
+  display_name          = var.display_name
+  description           = var.description
+}
