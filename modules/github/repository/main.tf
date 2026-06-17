9884b8d3fa5239ac1d8af5b5491f362b6796f5f4
AlexTheMan <alex.man03888@gmail.com>
2026-06-17 09:59:32 +0100

    Add priority labels
    Useful for filtering issues by importance.

--- modules/github/repository/main.tf
+++ modules/github/repository/main.tf
@@ -211,6 +211,42 @@ resource "github_issue_label" "high_risk" {
   description = "High risk change"
 }
 
+resource "github_issue_label" "low_priority" {
+  count = !var.archived ? 1 : 0
+
+  repository  = github_repository.default.name
+  name        = "low priority"
+  color       = "0E8A16"
+  description = "Low priority change"
+}
+
+resource "github_issue_label" "medium_priority" {
+  count = !var.archived ? 1 : 0
+
+  repository  = github_repository.default.name
+  name        = "medium priority"
+  color       = "FBCA04"
+  description = "Medium priority change"
+}
+
+resource "github_issue_label" "high_priority" {
+  count = !var.archived ? 1 : 0
+
+  repository  = github_repository.default.name
+  name        = "high priority"
+  color       = "D73A4A"
+  description = "High priority change"
+}
+
+resource "github_issue_label" "critical" {
+  count = !var.archived ? 1 : 0
+
+  repository  = github_repository.default.name
+  name        = "critical priority"
+  color       = "B60205"
+  description = "Critical priority change"
+}
+
 resource "github_issue_label" "default" {
   for_each = var.labels
 
