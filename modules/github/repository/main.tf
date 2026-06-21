29d77f77c1b0dc61f807478ada6697ff9f68c656
AlexMan123456 <alex.man03888@gmail.com>
2026-06-21 14:30:38 +0100

    Make the risk/priority labels organisation-level labels
    This should allow them to be shared more naturally with other repositories. Not 100% sure if this will work, though - check the plan before merging.

--- modules/github/repository/main.tf
+++ modules/github/repository/main.tf
@@ -184,69 +184,6 @@ resource "github_actions_variable" "default" {
   value         = each.value
 }
 
-resource "github_issue_label" "low_risk" {
-  count = !var.archived ? 1 : 0
-
-  repository  = github_repository.default.name
-  name        = "low risk"
-  color       = "008672"
-  description = "Low risk change"
-}
-
-resource "github_issue_label" "medium_risk" {
-  count = !var.archived ? 1 : 0
-
-  repository  = github_repository.default.name
-  name        = "medium risk"
-  color       = "7057FF"
-  description = "Medium risk change"
-}
-
-resource "github_issue_label" "high_risk" {
-  count = !var.archived ? 1 : 0
-
-  repository  = github_repository.default.name
-  name        = "high risk"
-  color       = "E4E669"
-  description = "High risk change"
-}
-
-resource "github_issue_label" "low_priority" {
-  count = !var.archived ? 1 : 0
-
-  repository  = github_repository.default.name
-  name        = "low priority"
-  color       = "0E8A16"
-  description = "Low priority change"
-}
-
-resource "github_issue_label" "medium_priority" {
-  count = !var.archived ? 1 : 0
-
-  repository  = github_repository.default.name
-  name        = "medium priority"
-  color       = "FBCA04"
-  description = "Medium priority change"
-}
-
-resource "github_issue_label" "high_priority" {
-  count = !var.archived ? 1 : 0
-
-  repository  = github_repository.default.name
-  name        = "high priority"
-  color       = "D73A4A"
-  description = "High priority change"
-}
-
-resource "github_issue_label" "critical_priority" {
-  count = !var.archived ? 1 : 0
-
-  repository  = github_repository.default.name
-  name        = "critical priority"
-  color       = "B60205"
-  description = "Critical priority change"
-}
-
 resource "github_issue_label" "default" {
   for_each = var.labels
 
