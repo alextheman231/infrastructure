a90e41701e991b996905f57d442a6d0e0089dda0
AlexMan123456 <alex.man03888@gmail.com>
2026-06-21 14:30:38 +0100

    Make the risk/priority labels organisation-level labels
    This should allow them to be shared more naturally with other repositories. Not 100% sure if this will work, though - check the plan before merging.

--- modules/github/organisation/main.tf
+++ modules/github/organisation/main.tf
@@ -60,3 +60,52 @@ resource "github_membership" "members" {
   username = each.value
   role     = "member"
 }
+
+resource "github_issue_label" "low_risk" {
+  repository  = var.name
+  name        = "low risk"
+  color       = "008672"
+  description = "Low risk change"
+}
+
+resource "github_issue_label" "medium_risk" {
+  repository  = var.name
+  name        = "medium risk"
+  color       = "7057FF"
+  description = "Medium risk change"
+}
+
+resource "github_issue_label" "high_risk" {
+  repository  = var.name
+  name        = "high risk"
+  color       = "E4E669"
+  description = "High risk change"
+}
+
+resource "github_issue_label" "low_priority" {
+  repository  = var.name
+  name        = "low priority"
+  color       = "0E8A16"
+  description = "Low priority change"
+}
+
+resource "github_issue_label" "medium_priority" {
+  repository  = var.name
+  name        = "medium priority"
+  color       = "FBCA04"
+  description = "Medium priority change"
+}
+
+resource "github_issue_label" "high_priority" {
+  repository  = var.name
+  name        = "high priority"
+  color       = "D73A4A"
+  description = "High priority change"
+}
+
+resource "github_issue_label" "critical_priority" {
+  repository  = var.name
+  name        = "critical priority"
+  color       = "B60205"
+  description = "Critical priority change"
+}
