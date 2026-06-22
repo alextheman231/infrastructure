b0ce79ceb38ea2ef50f2ba66aec1fa86509e4b4e
AlexMan123456 <alex.man03888@gmail.com>
2026-06-23 00:54:12 +0100

    Rename the ingress/egress rules in Terraform
    It just makes it clearer. It might destroy/recreate, but this is safe because they're just permissions.

--- modules/aws/bastion/main.tf
+++ modules/aws/bastion/main.tf
@@ -34,7 +34,7 @@ resource "aws_security_group" "bastion" {
   vpc_id = data.aws_vpc.default.id
 }
 
-resource "aws_vpc_security_group_ingress_rule" "default" {
+resource "aws_vpc_security_group_ingress_rule" "ssh" {
   for_each = var.allowed_ipv4s
 
   security_group_id = aws_security_group.bastion.id
@@ -47,7 +47,7 @@ resource "aws_vpc_security_group_ingress_rule" "default" {
   description = each.key
 }
 
-resource "aws_vpc_security_group_egress_rule" "default" {
+resource "aws_vpc_security_group_egress_rule" "all" {
   security_group_id = aws_security_group.bastion.id
 
   ip_protocol = "-1"
