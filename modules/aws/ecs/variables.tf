965586ba8ccf4de59eefde74793b00752cef4ea9
AlexMan123456 <alex.man03888@gmail.com>
2026-06-24 00:05:08 +0100

    Set up the ECS container for lexicon
    Because I don't think we can actually test the database URL directly in Render as it won't be able to access it without doing some hacky stuff in AWS that I really don't want to do.

---
+++ modules/aws/ecs/variables.tf
@@ -0,0 +1,33 @@
+variable "name" {
+  description = "The name of the service."
+  type        = string
+}
+
+variable "cpu" {
+  description = "Number of CPU units for the task."
+  type        = number
+  default     = 256
+}
+
+variable "memory" {
+  description = "Amount of memory used by the task in MiB."
+  type        = number
+  default     = 512
+}
+
+variable "image" {
+  description = "The image to deploy."
+  type        = string
+}
+
+variable "environment_variables" {
+  description = "The environment variables for the service."
+  sensitive   = true
+  type        = map(string)
+}
+
+variable "fargate_version" {
+  description = "The Fargate version"
+  type        = string
+  default     = "1.4"
+}
