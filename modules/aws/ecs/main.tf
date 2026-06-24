908b16cd2bb8153a2935ffd2728e5464596df74e
AlexMan123456 <alex.man03888@gmail.com>
2026-06-24 02:17:25 +0100

    Add an egress rule for the security group
    I think this should fix the connectivity errors we are getting.

--- modules/aws/ecs/main.tf
+++ modules/aws/ecs/main.tf
@@ -46,6 +46,13 @@ resource "aws_security_group" "ecs" {
   vpc_id = data.aws_vpc.default.id
 }
 
+resource "aws_vpc_security_group_egress_rule" "all" {
+  security_group_id = aws_security_group.ecs.id
+
+  ip_protocol = "-1"
+  cidr_ipv4   = "0.0.0.0/0"
+}
+
 resource "aws_ecs_cluster" "default" {
   name = var.name
 }
