ba1a640b1b82bf9c827b8be4919af5ab52f23f3a
AlexMan123456 <alex.man03888@gmail.com>
2026-06-27 00:32:14 +0100

    Add the ACM certificate resource and give Lexicon a certificate
    After this, we will need to validate the DNS, I think. I think it needs to be done in separate steps so the certificate is definitely created before we configure DNS.

--- modules/aws/alb/outputs.tf
+++ modules/aws/alb/outputs.tf
@@ -7,5 +7,13 @@ output "target_group_arn" {
 }
 
 output "listener_arn" {
-  value = aws_lb_listener.default.arn
+  value = aws_lb_listener.https.arn
+}
+
+output "dns_name" {
+  value = aws_lb.default.dns_name
+}
+
+output "zone_id" {
+  value = aws_lb.default.zone_id
 }
