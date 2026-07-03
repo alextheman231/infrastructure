d6dfe240bd7c4b2585f4fe44ae904f4eef3ba468
AlexMan123456 <alex.man03888@gmail.com>
2026-07-04 00:34:03 +0100

    Output the secret ARNs mapping from the secrets manager module
    This reduces duplication and keeps the secret manager as the main source of truth for secret values.

--- modules/aws/secrets_manager/outputs.tf
+++ modules/aws/secrets_manager/outputs.tf
@@ -2,3 +2,11 @@ output "arn" {
   description = "ARN of the secret."
   value       = aws_secretsmanager_secret.default.arn
 }
+
+output "secret_arns" {
+  description = "Mapping of secret names to the Secrets Manager ARN containing them."
+  value = {
+    for name in nonsensitive(keys(var.secrets)) :
+    name => aws_secretsmanager_secret.default.arn
+  }
+}
