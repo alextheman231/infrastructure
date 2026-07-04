2d15b97b24fa20f02b28d523bdb9c7128ea57a3e
AlexMan123456 <alex.man03888@gmail.com>
2026-07-04 16:42:43 +0100

    Add a NAT Gateway
    This allows the ECS service to use the private subnets.

--- modules/aws/network/resources.tf
+++ modules/aws/network/resources.tf
@@ -39,6 +39,26 @@ resource "aws_internet_gateway" "default" {
   }
 }
 
+resource "aws_eip" "nat" {
+  domain = "vpc"
+
+  tags = {
+    Name = "${var.name}-nat-ip"
+  }
+}
+
+resource "aws_nat_gateway" "default" {
+  allocation_id = aws_eip.nat.id
+  subnet_id     = aws_subnet.public[0].id
+
+  tags = {
+    Name = "${var.name}-nat-gateway"
+  }
+
+  depends_on = [aws_internet_gateway.default]
+}
+
+
 resource "aws_route_table" "public" {
   vpc_id = aws_vpc.default.id
 
@@ -51,11 +71,31 @@ resource "aws_route_table" "public" {
   }
 }
 
+resource "aws_route_table" "private" {
+  vpc_id = aws_vpc.default.id
+
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.default.id
+  }
+
+  tags = {
+    Name = "${var.name}-private-route-table"
+  }
+}
+
 resource "aws_route_table_association" "public_gateway" {
   count = length(aws_subnet.public)
 
   subnet_id      = aws_subnet.public[count.index].id
   route_table_id = aws_route_table.public.id
+}
+
+resource "aws_route_table_association" "private_gateway" {
+  count = length(aws_subnet.private)
+
+  subnet_id      = aws_subnet.private[count.index].id
+  route_table_id = aws_route_table.private.id
 }
 
 resource "aws_db_subnet_group" "default" {
