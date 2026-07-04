668dd6e28e91457b3128d31c21d045be5abceb37
AlexMan123456 <alex.man03888@gmail.com>
2026-07-04 02:03:57 +0100

    Move the validation records to the new Lexicon deployment
    They are actually identical between them both, so this can be moved over as is without needing to recreate the records.

--- services/lexicon/networking.tf
+++ services/lexicon/networking.tf
@@ -19,39 +19,12 @@ module "lexicon_acm_certificate" {
   subject_alternative_names = ["www.${var.lexicon_domain}"]
 }
 
-module "lexicon_dns_validation_records" {
-  for_each = {
-    for option in module.lexicon_acm_certificate.domain_validation_options :
-    option.domain_name => option
-  }
-
-  source = "../../modules/cloudflare/dns"
-
-  name    = each.value.resource_record_name
-  type    = each.value.resource_record_type
-  content = each.value.resource_record_value
-
-  zone_id = var.cloudflare_lexicon_zone_id
-  proxied = false
-  ttl     = 660
-}
-
-module "lexicon_acm_certificate_validation" {
-  source = "../../modules/aws/certificate_validation"
-
-  certificate_arn = module.lexicon_acm_certificate.certificate_arn
-  validation_record_fqdns = [
-    for record in module.lexicon_dns_validation_records :
-    record.fqdn
-  ]
-}
-
 module "lexicon_load_balancer" {
   source            = "../../modules/aws/alb"
   name              = "lexicon"
   health_check_path = "/api/v1"
   port              = local.backend_port
-  certificate_arn   = module.lexicon_acm_certificate_validation.validated_certificate_arn
+  certificate_arn   = module.lexicon_acm_certificate.certificate_arn
   vpc_id            = data.aws_vpc.default.id
   subnet_ids        = data.aws_subnets.default.ids
 }
