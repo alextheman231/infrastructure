046913dbddf810802c533d52fefb7c51fc98e35c
AlexMan123456 <alex.man03888@gmail.com>
2026-07-04 18:35:35 +0100

    Add moved block for previous task execution role
    Just to be safe - probably doesn't matter much in theory, but good practice to add anyway.

--- services/lexicon/main.tf
+++ services/lexicon/main.tf
@@ -18,3 +18,8 @@ terraform {
     }
   }
 }
+
+moved {
+  from = module.lexicon_ecs_service.aws_iam_role.ecs_task_execution
+  to   = module.lexicon_ecs_task_execution_role.aws_iam_role.ecs_task_execution
+}
