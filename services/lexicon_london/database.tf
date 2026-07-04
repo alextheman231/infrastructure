667eb85139fce2b9600521e95ad59498ba634a12
AlexMan123456 <alex.man03888@gmail.com>
2026-07-04 15:19:14 +0100

    Finally destroy the London Lexicon database for real this time I swear it will work

--- services/lexicon_london/database.tf
+++ services/lexicon_london/database.tf
@@ -1,12 +1,12 @@
-module "lexicon_database" {
-  source                    = "../../modules/aws/database"
-  initial_db_name           = "lexicon"
-  db_identifier             = "lexicon-prod"
-  postgres_version          = "18"
-  username                  = "lexicon_user"
-  password                  = var.lexicon_database_password
-  bastion_security_group_id = module.lexicon_bastion.security_group_id
-  vpc_id                    = module.lexicon_network.vpc_id
-  db_subnet_group_name      = module.lexicon_network.public_subnet_group_name
-  deletion_protection       = false
-}
+# module "lexicon_database" {
+#   source                    = "../../modules/aws/database"
+#   initial_db_name           = "lexicon"
+#   db_identifier             = "lexicon-prod"
+#   postgres_version          = "18"
+#   username                  = "lexicon_user"
+#   password                  = var.lexicon_database_password
+#   bastion_security_group_id = module.lexicon_bastion.security_group_id
+#   vpc_id                    = module.lexicon_network.vpc_id
+#   db_subnet_group_name      = module.lexicon_network.public_subnet_group_name
+#   deletion_protection       = false
+# }
