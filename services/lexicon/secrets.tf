76691d162959615a7f2374819543abdc7afb1ae6
AlexMan123456 <alex.man03888@gmail.com>
2026-07-04 21:29:16 +0100

    Destroy the Stockholm Lexicon database
    And this time, unlike the London Lexicon database, it will not be recreated.

--- services/lexicon/secrets.tf
+++ services/lexicon/secrets.tf
@@ -2,7 +2,6 @@ module "lexicon_secrets" {
   source = "../../modules/aws/secrets_manager"
   name   = "lexicon"
   secrets = {
-    DATABASE_URL         = module.lexicon_database.database_url
     GOOGLE_CLIENT_SECRET = var.lexicon_google_client_secret
   }
 }
