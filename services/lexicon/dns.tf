b4f1a96afac5291361e7d1586aa08fe03d71be06
AlexMan123456 <alex.man03888@gmail.com>
2026-07-05 02:17:52 +0100

    Refactor the DNS records so that we create both the ww record and the main record both at a time

--- services/lexicon/dns.tf
+++ services/lexicon/dns.tf
@@ -1,16 +1,11 @@
 module "lexicon_dns_record" {
   source = "../../modules/cloudflare/dns"
+  for_each = toset([
+    var.lexicon_domain,
+    "www.${var.lexicon_domain}"
+  ])
 
-  name    = var.lexicon_domain
-  type    = "CNAME"
-  zone_id = var.cloudflare_lexicon_zone_id
-  content = module.lexicon_load_balancer.dns_name
-}
-
-module "lexicon_dns_record_www" {
-  source = "../../modules/cloudflare/dns"
-
-  name    = "www.${var.lexicon_domain}"
+  name    = each.value
   type    = "CNAME"
   zone_id = var.cloudflare_lexicon_zone_id
   content = module.lexicon_load_balancer.dns_name
