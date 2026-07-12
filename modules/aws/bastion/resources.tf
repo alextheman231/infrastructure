1a6889da82833472ed15ef541e3ea31e7ca7e847
AlexMan123456 <alex.man03888@gmail.com>
2026-07-12 23:57:17 +0100

    Add user_data to the Bastion
    This should give us a nice base setup that will always exist in the Bastion should it ever get recreated (e.g. due to an AMI update).

--- modules/aws/bastion/resources.tf
+++ modules/aws/bastion/resources.tf
@@ -17,6 +17,14 @@ resource "aws_instance" "bastion" {
   vpc_security_group_ids = var.security_group_ids
 
   key_name = aws_key_pair.bastion.key_name
+  user_data = templatefile("${path.module}/templates/bastion_user_data.sh.tftpl", {
+    postgres_version = var.postgres_version
+    database_address = var.database_address
+    port             = var.port
+    database_user    = var.database_user
+    database_name    = var.database_name
+  })
+  user_data_replace_on_change = true
 
   tags = {
     Name = var.name
--- modules/aws/bastion/variables.tf
+++ modules/aws/bastion/variables.tf
@@ -17,3 +17,29 @@ variable "security_group_ids" {
   description = "List of security group IDs to associate with the Bastion"
   type        = list(string)
 }
+
+variable "postgres_version" {
+  description = "The Postgres version"
+  type        = string
+  default     = "18"
+}
+
+variable "database_address" {
+  description = "The database host address."
+  type        = string
+}
+
+variable "port" {
+  description = "The database connection port."
+  type        = string
+}
+
+variable "database_user" {
+  description = "The username of the user to connect to the database with."
+  type        = string
+}
+
+variable "database_name" {
+  description = "The name of the database."
+  type        = string
+}
