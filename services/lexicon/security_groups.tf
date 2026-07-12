21cba9d53835104682824b818bbba7226d4470bf
AlexTheMan <alex.man03888@gmail.com>
2026-07-12 12:38:57 +0100

    Remove moved blocks after successful move

--- services/lexicon/security_groups.tf
+++ services/lexicon/security_groups.tf
@@ -19,17 +19,3 @@ module "lexicon_load_balancer_security_group" {
   vpc_id = module.lexicon_network.vpc_id
 }
 
-moved {
-  from = module.lexicon_database.aws_security_group.database
-  to   = module.lexicon_database_security_group.aws_security_group.default
-}
-
-moved {
-  from = module.lexicon_bastion.aws_security_group.bastion
-  to   = module.lexicon_bastion_security_group.aws_security_group.default
-}
-
-moved {
-  from = module.lexicon_load_balancer.aws_security_group.alb
-  to   = module.lexicon_load_balancer_security_group.aws_security_group.default
-}
