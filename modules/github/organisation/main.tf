c58a030815cc1acb0a3746a8f307d9593cebac81
AlexMan123456 <alex.man03888@gmail.com>
2026-07-21 23:31:40 +0100

    Move the GitHub organisation settings resource into resources.tf
    More consistent with our patterns elsewhere.

--- modules/github/organisation/main.tf
+++ modules/github/organisation/main.tf
@@ -9,15 +9,3 @@ terraform {
   }
 }
 
-resource "github_organization_settings" "default" {
-  billing_email                           = var.billing_email
-  name                                    = var.name
-  description                             = var.description
-  default_repository_permission           = "write"
-  members_can_create_public_repositories  = false
-  members_can_create_private_repositories = false
-
-  lifecycle {
-    ignore_changes = [members_can_create_repositories]
-  }
-}
