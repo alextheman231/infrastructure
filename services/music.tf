2372aec266e0f4880f2d38ea70bc52dfe1a21817
AlexMan123456 <alex.man03888@gmail.com>
2026-07-21 01:25:55 +0100

    Add a music repository to store my music projects
    I have been given way too much power at this point...

---
+++ services/music.tf
@@ -0,0 +1,34 @@
+module "music_repository" {
+  source = "../modules/github/repository"
+
+  name               = "music"
+  description        = "My music projects."
+  visibility         = "public"
+  alex_up_bot_app_id = var.alex_up_bot_app_id
+  labels = {
+    "breaking change" = {
+      color       = "B60205"
+      description = "Require a drastic change in how performers consume the song."
+    }
+    "new section" = {
+      color       = "A2EEEF"
+      description = "Adds a new section to a song"
+    }
+    "wrong note fix" = {
+      color       = "D73A4A"
+      description = "Fixes a misplaced note."
+    }
+    "timbre change" = {
+      color       = "5319E7"
+      description = "Changes to the instrumentation and timbre."
+    }
+    "mixing and mastering" = {
+      color       = "28A745"
+      description = "Changes to the mixing/mastering."
+    }
+    "refactor" = {
+      color       = "C2E0C6"
+      description = "Quality improvements without audible changes."
+    }
+  }
+}
