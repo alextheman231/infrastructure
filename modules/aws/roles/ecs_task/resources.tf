dc29cbc73e2beec3507b88cf20c2a844883656c1
AlexMan123456 <alex.man03888@gmail.com>
2026-07-25 21:22:25 +0100

    Fix the version
    I think it's meant to be 2012-10-17, not 2012-10-27...

--- modules/aws/roles/ecs_task/resources.tf
+++ modules/aws/roles/ecs_task/resources.tf
@@ -21,7 +21,7 @@ resource "aws_iam_policy" "ecs_task_s3_access" {
   description = "Policy allowing ECS tasks to access S3 buckets."
 
   policy = jsonencode({
-    Version = "2012-10-27"
+    Version = "2012-10-17"
     Statement = [
       {
         Action = [
