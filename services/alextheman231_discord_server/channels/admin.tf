87f30b0d7ba0db223e52168dd46c5baee43804d7
AlexMan123456 <alex.man03888@gmail.com>
2026-07-27 02:05:41 +0100

    Do not control positioning of text channels in Terraform
    I think that's probably best managed by UI, otherwise we can get drift.

--- services/alextheman231_discord_server/channels/admin.tf
+++ services/alextheman231_discord_server/channels/admin.tf
@@ -2,12 +2,10 @@ resource "discord_text_channel" "welcome_leave" {
   name      = "welcome-leave"
   category  = var.admin_category_id
   server_id = var.server_id
-  position  = 0
 }
 
 resource "discord_text_channel" "invite_link" {
   name      = "invite-link"
   category  = var.admin_category_id
   server_id = var.server_id
-  position  = 1
 }
