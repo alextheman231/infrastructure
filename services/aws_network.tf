9c07147ae8926103c1eb2382c1d19d7c9afacb51
AlexMan123456 <alex.man03888@gmail.com>
2026-08-02 00:26:24 +0100

    Well that apply was a s***show but at least it is done now!

--- services/aws_network.tf
+++ services/aws_network.tf
@@ -2,13 +2,3 @@ module "aws_network" {
   source = "../modules/aws/network"
   name   = "alextheman231"
 }
-
-moved {
-  from = module.lexicon.module.lexicon_network
-  to   = module.aws_network
-}
-
-moved {
-  from = module.aws_network.aws_db_subnet_group.default
-  to   = module.lexicon.module.lexicon_database.aws_db_subnet_group.default
-}
