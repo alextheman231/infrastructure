d4329f2056b5b2c2609288c489cc746299410818
AlexMan123456 <alex.man03888@gmail.com>
2026-08-05 23:29:57 +0100

    Move the secrets manager role policy inside the secrets manager itself
    This allows the module itself to handle role permissions natively, rather than call sites needing to wire it up themselves. It also means that we can enforce that the plan role must be passed into the secrets manager, which is needed otherwise the plan after the apply will fail due to insufficient permissions.

--- modules/aws/roles/terraform/outputs.tf
+++ modules/aws/roles/terraform/outputs.tf
@@ -2,6 +2,10 @@ output "role_name" {
   value = aws_iam_role.role.name
 }
 
+output "role_id" {
+  value = aws_iam_role.role.id
+}
+
 output "role_arn" {
   value = aws_iam_role.role.arn
 }
--- modules/aws/secrets_manager/resources.tf
+++ modules/aws/secrets_manager/resources.tf
@@ -7,3 +7,26 @@ resource "aws_secretsmanager_secret_version" "default" {
   secret_id     = aws_secretsmanager_secret.default.id
   secret_string = jsonencode(var.secrets)
 }
+
+resource "aws_iam_role_policy" "secret_access" {
+  for_each = merge(var.allowed_role_ids, {
+    terraform_plan_role = var.plan_role_id
+  })
+
+  name = "${each.key}-${var.name}-secrets-access"
+  role = each.value
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+
+    Statement = [{
+      Effect = "Allow"
+
+      Action = [
+        "secretsmanager:GetSecretValue"
+      ]
+
+      Resource = aws_secretsmanager_secret.default.arn
+    }]
+  })
+}
