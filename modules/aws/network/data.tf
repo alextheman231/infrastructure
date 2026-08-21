7f2ad83085f2fc3d3b37fc09f2fbad6065a73415
AlexMan123456 <alex.man03888@gmail.com>
2026-08-21 21:57:46 +0100

    Attempt to fix the availability zones causing state drift
    There is a very bad state drift happening right now where the availability zones data source is changing the exact subnets we are using, which is also then planning to recreate pretty much the entire AWS setup. The hope is that adding these extra filters will help it be more specific and hopefully match what it was before.

--- modules/aws/network/data.tf
+++ modules/aws/network/data.tf
@@ -1,3 +1,13 @@
 data "aws_availability_zones" "available" {
   state = "available"
+
+  filter {
+    name   = "zone-type"
+    values = ["availability-zone"]
+  }
+
+  filter {
+    name   = "opt-in-status"
+    values = ["opt-in-not-required"]
+  }
 }
