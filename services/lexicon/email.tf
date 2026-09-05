c5170c062ade9cacf8541f2827a06babbe394f7c
AlexMan123456 <alex.man03888@gmail.com>
2026-09-05 17:45:09 +0100

    Add the DNS records for email sending using the Lexicon domain
    So that lexiconblogs.com can be used as a domain for email addresses.

--- services/lexicon/email.tf
+++ services/lexicon/email.tf
@@ -3,3 +3,16 @@ module "ses_identity" {
 
   domain_name = var.lexicon_domain
 }
+
+module "ses_dkim_records" {
+  for_each = toset(module.ses_identity.dkim_tokens)
+  source   = "../../modules/cloudflare/dns"
+
+  name    = "${each.value}._domainkey.${var.lexicon_domain}"
+  type    = "CNAME"
+  content = "${each.value}.dkim.amazonses.com"
+
+  zone_id = data.cloudflare_zone.lexicon_domain.zone_id
+  proxied = false
+  ttl     = 660
+}
