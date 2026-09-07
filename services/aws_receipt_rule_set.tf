11d3509f8a6dc8cfe22ec3406c49fde2f146c2c5
AlexMan123456 <alex.man03888@gmail.com>
2026-09-08 00:43:31 +0100

    Create the receipt rule set before activating it
    The active_receipt_rule_set resource is meant to activate a given receipt rule set. It is similar to acm_certificate_validation, which validates the provided certificate.

--- services/aws_receipt_rule_set.tf
+++ services/aws_receipt_rule_set.tf
@@ -1,3 +1,7 @@
-resource "aws_ses_active_receipt_rule_set" "default" {
+resource "aws_ses_receipt_rule_set" "default" {
   rule_set_name = "alextheman231"
+}
+
+resource "aws_ses_active_receipt_rule_set" "default" {
+  rule_set_name = aws_ses_receipt_rule_set.default.rule_set_name
 }
