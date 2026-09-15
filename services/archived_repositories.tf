02d426d5e6f7b4539ee4014e6e144a6862a61463
AlexMan123456 <alex.man03888@gmail.com>
2026-09-16 00:29:08 +0100

    Archive the typescript-actions repository
    It has been officially merged with the github-actions repository, which contains both the reusable workflows and the TypeScript composite actions.

--- services/archived_repositories.tf
+++ services/archived_repositories.tf
@@ -17,3 +17,14 @@ module "alex_g_bot_2_repository" {
   required_ci_checks = concat(local.check_list.base, [local.check_name.alex_g_bot.ci])
   alex_up_bot_app_id = var.alex_up_bot_app_id
 }
+
+module "typescript_actions_repository" {
+  source             = "../modules/github/repository"
+  name               = "typescript-actions"
+  description        = "Composite actions developed in TypeScript to use in GitHub Actions workflows."
+  visibility         = "public"
+  archived           = true
+  required_ci_checks = concat(local.check_list.base, [local.check_name.package.source_code_ci])
+  alex_up_bot_app_id = var.alex_up_bot_app_id
+  labels             = merge(local.labels.standard, local.labels.package)
+}
